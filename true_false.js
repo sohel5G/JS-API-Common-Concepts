44c10f31e7f473514fdcbfc1b12fc9482d197ae1
sohel5G <contactsohelrana@gmail.com>
2023-08-26 23:28:31 +0600

    2 video done

---
+++ true_false.js
@@ -0,0 +1,28 @@
+// // Truthy 
+//     true
+//     4 or -4
+//     'any string' or ' ' 1ta kali string also truthy
+//     '0'
+
+
+// // falsy 
+//     false
+//     '' Just a empty string is false 
+//     0 
+
+
+
+const x = '0';
+
+if (x) {
+    console.log('This is truthy');
+} else {
+    console.log('This is falsy');
+}
+
+
+
+
+
+
+
---
+++ undefined.js
@@ -0,0 +1,32 @@
+
+// 8 way to get undefined
+
+// 1
+let varname; // it will be undefined
+
+// 2
+function funcName(a, b) {
+    const total = a + b;
+}
+console.log(funcName());
+
+// 3
+// Function er parameter na dele undefined dekabe
+
+// 4
+// Function er vithor just return like dele undefined asbe, ki return hobe seta bole na dele
+
+// 5
+// Kuno object er property jodi na take then undefined debe
+
+// 6
+// array er index jodi na take but access korte sau taile undefined debe  ex: arr[24] 
+
+// 7
+// Array teke kuno value delete kore dele undefined debe 
+
+// Kuno data set korte hole null deye set koray better 
+
+
+console.log( typeof null );
+
